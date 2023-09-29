aeedf0af63ef215481adc61c85275d59789622ba
Suganda <129074317+suganda02@users.noreply.github.com>
2023-09-29 22:58:33 +0700

    Add files via upload

---
+++ Tugas1.js
@@ -0,0 +1,22 @@
+// Identitas
+const nama = "SUGANDA";
+const nim = "20220040258";
+
+console.log("Identitas : ");
+console.log("Nama : " + nama);
+console.log("Nim : " + nim);
+
+// Operasi Aritmatika Sederhana
+const angka1 = 17;
+const angka2 = 6;
+
+const tambah = angka1 + angka2;
+const kurang = angka1 - angka2;
+const kali = angka1 * angka2;
+const bagi = angka1 / angka2;
+
+console.log("Operasi Aritmatika : ");
+console.log(${angka1} + ${angka2} = ${tambah});
+console.log(${angka1} - ${angka2} = ${kurang});
+console.log(${angka1} * ${angka2} = ${kali});
+console.log(${angka1} / ${angka2} = ${bagi});
